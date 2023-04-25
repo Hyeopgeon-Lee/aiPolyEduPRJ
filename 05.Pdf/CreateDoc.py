0b5e8b584618a1bc9ed94bb49630fea90eaf14f9
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2023-04-25 14:03:06 +0900

    워드 파일 생성 소스 추가

--- 05.Pdf/CreateDoc.py
+++ 05.Pdf/CreateDoc.py
@@ -1,10 +1,10 @@
 from pdf2docx import Converter
 
 # PDF 내 문장을 가져오기
-cv = Converter("../pdf/novel1.pdf")
+cv = Converter("../pdf/report1.pdf")
 
 # 가져온 문장을 docx 파일로 변환하기
-cv.convert("../docx/novel1.docx")
+cv.convert("../docx/report1.docx")
 
 # 사용 완료하여 닫기
 cv.close()
