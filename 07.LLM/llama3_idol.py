1d5ca31fb27c67830568d177cbb6e38688ca178d
Hyeopgeon Lee <soft1318@naver.com>
2024-04-29 14:54:55 +0900

    LLAMA3 예제 추가

--- 07.LLM/llama3_idol.py
+++ 07.LLM/llama3_idol.py
@@ -2,8 +2,10 @@ import ollama
 
 stream = ollama.chat(  # 라마3로부터 생성된 답변을 실시간 받기
     model="llama3",  # 사용 모델
-    messages=[{"role": "user", "content": "K-POP 그룹인 'LE SSERAFIM'과  'ITZY' 중 누가 더 팬들에게 인기 많을까?"},
-              {"role": "user", "content": "한글로 번역해서 알려줘"}],  # 명령어
+    messages=[{"role": "user",
+               "content": "2023년 기준으로 K-POP 그룹인 'LE SSERAFIM'과  'ITZY' 중 누가 더 팬들에게 인기 많을까?"},
+              {"role": "user",
+               "content": "한글로 번역해서 알려줘"}],  # 명령어
     stream=True,  # 응답결과 실시간 받기 설정
 )
 
